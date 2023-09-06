b7c2def8d03894b62fad5bda69e35ba751c2f0f3
hewsontrinh526 <6734@holbertonstudents.com>
2023-09-06 00:38:11 -0700

    Feat: Checks if a node is a parent

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -13,15 +13,9 @@ int binary_tree_is_root(const binary_tree_t *node)
 	{
 		return (0);
 	}
-	binary_tree_is_root(node->left);
-	if ((node->left != NULL) && (node->right != NULL))
+	if (node->parent == NULL)
 	{
-		if (node->parent == NULL)
-		{
-			return (1);
-		}
-		return (0);
+		return (1);
 	}
-	binary_tree_is_root(node->right);
 	return (0);
 }
