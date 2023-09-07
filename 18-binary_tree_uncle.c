dc95bb45b23fe7e753685ac8440beba04e41a0d5
root <6721@holbertonstudents.com>
2023-09-07 02:10:21 -0700

    style: betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_siblings - finds the sibling of a node
+* binary_tree_sibling - finds the sibling of a node
 * @node: pointer to the node to check
 *
 * Return: returns the sibling, else NULL
